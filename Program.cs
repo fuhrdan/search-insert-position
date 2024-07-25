77218915427047213e3b5c0716a6f5f158ce1e8c
fuhrdan <165685619+fuhrdan@users.noreply.github.com>
2024-07-25 10:28:30 -0600

    first commit

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿//*****************************************************************************
+//** 35. Search Insert Position leetcode                                     **
+//** For loop with an if statement, beats 100% in C                     -Dan **
+//*****************************************************************************
+
+
+int searchInsert(int* nums, int numsSize, int target) {
+    for (int i = 0; i < numsSize; i++)
+    {
+//        printf("target = %d and nums[%d]=%d\n",target,i,nums[i]);
+        if((target == nums[i])||(target < nums[i])) return i;
+    }
+    return numsSize;
+}
